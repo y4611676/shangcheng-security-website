53816069098afc4e422ba23b23c0eee99e76c9cd
y4611676 <71640831+y4611676@users.noreply.github.com>
2026-01-06 09:29:08 +0800

    更新2

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -77,7 +77,7 @@ export default function Contact() {
               LINE 聯絡
             </a>
           </div>
-        </div>
+        </header>
 
         {/* Contact Info Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-12">
